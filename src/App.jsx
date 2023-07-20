4f288f809fb24c8c5f2d9ddc6c637a41d7c6c200
Alskiir <doughart215@cfl.rr.com>
2023-07-20 13:56:45 -0400

    Refractor

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react"
 import Sidebar from "./components/Sidebar"
 import Editor from "./components/Editor"
-// import { data } from "./data"
 import Split from "react-split"
 import {nanoid} from "nanoid"
 
@@ -10,7 +9,7 @@ export default function App() {
         () => JSON.parse(localStorage.getItem("notes")) || []
     )
     const [currentNoteId, setCurrentNoteId] = React.useState(
-        (notes[0] && notes[0].id) || ""
+        (notes[0]?.id) || ""
     )
     
     function createNewNote() {
@@ -21,6 +20,14 @@ export default function App() {
         setNotes(prevNotes => [newNote, ...prevNotes])
         setCurrentNoteId(newNote.id)
     }
+
+    const currentNote = 
+        notes.find(note => note.id === currentNoteId) 
+        || notes[0]
+
+    React.useEffect(() => {
+        localStorage.setItem("notes", JSON.stringify(notes));
+      }, [notes]);
     
     function updateNote(text) {
         /* Puts the most recently modified note
@@ -39,19 +46,9 @@ export default function App() {
         })
       }
 
-    React.useEffect(() => {
-        localStorage.setItem("notes", JSON.stringify(notes));
-      }, [notes]);
-
     function deleteNote(event, noteId) {
         event.stopPropagation()
         setNotes(oldNotes => oldNotes.filter((note) => note.id !== noteId))
-    }
-    
-    function findCurrentNote() {
-        return notes.find(note => {
-            return note.id === currentNoteId
-        }) || notes[0]
     }
     
     return (
@@ -66,7 +63,7 @@ export default function App() {
             >
                 <Sidebar
                     notes={notes}
-                    currentNote={findCurrentNote()}
+                    currentNote={currentNote}
                     setCurrentNoteId={setCurrentNoteId}
                     newNote={createNewNote}
                     deleteNote={deleteNote}
@@ -75,7 +72,7 @@ export default function App() {
                     currentNoteId && 
                     notes.length > 0 &&
                     <Editor 
-                        currentNote={findCurrentNote()} 
+                        currentNote={currentNote} 
                         updateNote={updateNote} 
                     />
                 }
